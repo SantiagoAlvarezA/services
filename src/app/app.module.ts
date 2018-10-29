4b9d045acb9c995b66a17c8540131da7a5902d03
Santiago Alvarez <aalvarez3570@gmail.com>
2018-10-29 13:07:02 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import {HttpClientModule} from '@angular/common/http';
+import{PostsService} from './services/posts.service'
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ListStudentsComponent } from './list-students/list-students.component';
+import { DetailStudentComponent } from './detail-student/detail-student.component';
+import { RegistrationStudentComponent } from './registration-student/registration-student.component';
+
+const appRoutes:Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'listStudents', component: ListStudentsComponent},
+    {path: 'detailStudent/:idEst', component: DetailStudentComponent},
+    {path: 'registrationStudent', component: RegistrationStudentComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        ListStudentsComponent,
+        DetailStudentComponent,
+        RegistrationStudentComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [PostsService],//adicion del servicio
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+    home:string = 'Inicio';
+    listStudents:string = 'Lista de estudiantes';
+    registrationStudent:string = 'Registro de estudiantes';
+    search:string = 'Buscar';
+    constructor() { }
+    ngOnInit() { }
+}
